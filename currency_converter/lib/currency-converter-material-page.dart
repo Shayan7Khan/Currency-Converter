ac86caf3d27861d7946cd16fd7df1b4d0230ddeb
Shayan Khan <shayan.khan12115@gmail.com>
2024-12-03 22:01:40 +0500

    Committed from PC

--- currency_converter/lib/currency-converter-material-page.dart
+++ currency_converter/lib/currency-converter-material-page.dart
@@ -5,9 +5,72 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    final border = OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                    ),
+                    borderRadius: BorderRadius.circular(5));
+
+
+    return  Scaffold(
+      backgroundColor:const  Color.fromARGB(255, 48, 94, 117),
       body: Center(
-        child: Text('Hello World !'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //TEXT/OUPUT
+             const Text('0', style:
+             TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)
+              ),
+              ),
+
+              //INPUT PLACEHOLDER
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black
+                ),
+                decoration: InputDecoration(
+                  hintText:'Please Enter The Amount In USD', hintStyle: const TextStyle(
+                    color: Colors.black
+                    ),
+                    prefixIcon: const Icon(Icons.monetization_on_outlined),
+                    prefixIconColor: Colors.black,
+                    //filled give us the placeholder
+                    filled: true,
+                    //fillcolor gives the color to the placeholder
+                    fillColor: Colors.white,
+                    //focusedborder is for the border which we can style give colors etc
+                    focusedBorder: border,
+                    enabledBorder: border,
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
+                      decimal: true,
+                    ),
+              ),
+            ),
+
+            //BUTTON
+            TextButton(onPressed: (){
+              debugPrint('Button Clicked');
+            },
+            style: const ButtonStyle(
+              backgroundColor:WidgetStatePropertyAll(Colors.black),
+              foregroundColor: WidgetStatePropertyAll(Colors.white)
+            ),
+             child: const Text('Convert'),
+             
+             )
+          ],
+        ),
       ),
     );
   }
